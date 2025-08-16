413daf6ebbac9a0afdc81fe3a5400b657138b5d9
Ravi chandu <92777166+ravi-chandu@users.noreply.github.com>
2025-08-16 15:32:00 +0530

    Update main.jsx

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,10 +1,15 @@
+// app/src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* Wrap your App in Router */}
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
